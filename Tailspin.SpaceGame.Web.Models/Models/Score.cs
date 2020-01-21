6bdcb7a033fe11cf0584b907bc309a9af89f4038
Realruja <=>
2020-01-21 15:10:06 -0500

    Add GameStyle Property

--- Tailspin.SpaceGame.Web.Models/Models/Score.cs
+++ Tailspin.SpaceGame.Web.Models/Models/Score.cs
@@ -21,7 +21,7 @@ namespace TailSpin.SpaceGame.Web.Models
         public string GameRegion { get; set; }
 
          // The game style (difficulty) the score is associated with.
-        [JsonProperty(PropertyName = "gameStyle")]
+        [JsonProperty(PropertyName = "gameStyle")] 
         public string GameStyle { get; set; }
     }
 }
